fb6bbb7cc80e853d985a9070f20908039ba62cb2
Dori <qarkchanjin@gmail.com>
2024-03-31 16:11:07 +0900

    Feat: Player[Cam Rotate & Move]

---
+++ Assets/Script/Player/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    private CharacterController cc;
+
+    private float gravity = -20f;
+    private float yVelocity = 0f;
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); // 좌 우
+        float v = Input.GetAxis("Vertical"); // 앞 뒤
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        dir = dir.normalized; //방향 정규화
+
+        //카메라 기준으로 방향을 지정 [기준을 명확하게]
+        dir = Camera.main.transform.TransformDirection(dir);
+        
+        // 캐릭터 수직 속도에 중력값 적용
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/Player/PlayerRotate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    [SerializeField] private float rotSpeed = 200f;
+    
+    //유니티 특성상 0~360으로 값을 처리 => -1도가 되는 경우 359도로 자동 연산 처리
+    //회전값 변수 -> 회전 값 누적을 위함.
+    private float mx = 0;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        // 플레이어의 경우 좌우 회전만 하면 된다. [범위는 따로 정할 필요 없음]
+        float mouse_X = Input.GetAxis("Mouse X"); // 마우스 x 축에 관한 입력 
+
+        //마우스 입력값 만큼 누적
+        mx += mouse_X * rotSpeed * Time.deltaTime;
+        
+        // 회전 방향으로 물체를 회전
+        transform.eulerAngles = new Vector3(0, mx, 0);
+    }
+}
